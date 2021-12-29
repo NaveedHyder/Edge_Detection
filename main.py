3643f7aacfcbe2fc3a51169b02f6785c5fc972ac
NAVEED HYDER <70444911+NaveedHyder@users.noreply.github.com>
2021-12-29 11:26:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+camera = cv.VideoCapture(0)    # 'dgsfsd.mp4'
+
+while True:
+    _, frame = camera.read()
+
+    laplacian = cv.Laplacian(frame, cv.CV_64F)
+    laplacian = np.uint8(laplacian)
+    #cv.imshow('Laplacian', laplacian)
+
+    edges = cv.Canny(frame, 50, 50)
+    cv.imshow('Canny', edges)
+
+    if cv.waitKey(5) == ord('x'):
+        break
+
+camera.release()
+cv.destroyAllWindows()
